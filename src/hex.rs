51e131614de11501c22d723946167fb9d1eebf33
Romain Cahuet <romain.cahuet@epita.fr>
2020-07-22 14:35:20 +0200

    [HEX][ADD] hex: added constructors and stores as vec

--- src/hex.rs
+++ src/hex.rs
@@ -1,12 +1,48 @@
+#[derive(Clone, Copy, PartialEq, Eq)]
 struct Hex {
-    q: i32,
-    r: i32,
-    s: i32,
+    coords: [i32; 3],
 }
 
 impl Hex {
-    fn new(q: i32, r: i32, s: i32) -> Hex {
-        assert!(q + r + s, 0, "Initiation of Hex incorrect, q + r + s must equal 0");
-        Hex(q, r, s)
+    fn new(q: i32, r: i32, s: i32) -> Self {
+        assert_eq!(
+            q + r + s,
+            0,
+            "Initiation of Hex incorrect, q + r + s must equal 0"
+        );
+        Hex { coords: [q, r, s] }
+    }
+
+    fn new2(q: i32, r: i32) -> Self {
+        Hex {
+            coords: [q, r, -q - r],
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Initiation of Hex incorrect, q + r + s must equal 0")]
+    fn new_3_incorrect() {
+        Hex::new(1, 2, 3);
+    }
+
+    #[test]
+    fn new_3_correct() {
+        let that: Hex = Hex::new(1, 2, -3);
+        assert_eq!(that.coords[0], 1);
+        assert_eq!(that.coords[1], 2);
+        assert_eq!(that.coords[2], -3);
+    }
+
+    #[test]
+    fn new_2() {
+        let that: Hex = Hex::new2(1, 2);
+        assert_eq!(that.coords[0], 1);
+        assert_eq!(that.coords[1], 2);
+        assert_eq!(that.coords[2], -3);
     }
 }
